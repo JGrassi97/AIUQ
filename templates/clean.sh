0b75efbb78c657e68c32989c293599961c7a7899
Jacopo Grassi <jacopo.grassi@polito.it>
2026-01-30 15:02:45 +0100

    Update clean.sh

--- templates/clean.sh
+++ templates/clean.sh
@@ -6,6 +6,7 @@ ICS_TEMP_DIR="%HPCROOTDIR%/ics_temp/%MODEL.NAME%/%SDATE%/ics/"
 # Delete the folder if it exists
 if [ -d "${INI_DATA_PATH}" ]; then
     rm -rf "${INI_DATA_PATH}"
+fi
 
 if [ -d "${ICS_TEMP_DIR}" ]; then
     rm -rf "${ICS_TEMP_DIR}"
