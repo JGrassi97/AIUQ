4e881d4c2304dac1ff71936183b0c1ab12a6a5e4
Jacopo Grassi <jacopo.grassi@polito.it>
2026-02-06 13:18:27 +0100

    -

--- templates/postprocess_aifs.sh
+++ templates/postprocess_aifs.sh
@@ -12,17 +12,20 @@ logs_dir=${HPCROOTDIR}/LOG_${EXPID}
 configfile=$logs_dir/config_${JOBNAME_WITHOUT_EXPID}
 PLATFORM_NAME=%PLATFORM.NAME%
 
+OUTPUT_PATH=%HPCROOTDIR%/outputs
+GRID_FILE=%PATHS.SUPPORT_FOLDER%/aifs_grid.txt
+
 # Load Singularity module only on MareNostrum5
 if [ "$PLATFORM_NAME" = "MARENOSTRUM5" ]; then
-    ml singularity
+    ml cdo
 fi
 
-EARTHKIT_CACHE=${HPCROOTDIR}/earthkit_cache
-export EARTHKIT_REGRID_CACHE=EARTHKIT_CACHE
+for f in $(find ${OUTPUT_PATH} -type f -name "*_temp.nc"); do
+    gridf=${f%_temp.nc}_grid.nc
+    outf=${f%_temp.nc}.nc
 
-singularity exec --nv \
-    --bind $HPCROOTDIR \
-    --env HPCROOTDIR=$HPCROOTDIR \
-    --env configfile=$configfile \
-    ${SIF_PATH} \
-    python3 $HPCROOTDIR/runscripts/postprocess_aifs.py -c $configfile
+    cdo -P 8 -setgrid,${GRID_FILE} ${f} ${gridf}
+    cdo -P 8 -f nc4 remapdis,r360x181 ${gridf} ${outf}
+
+    rm -f ${f} ${gridf}
+done
