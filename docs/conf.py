1eb4062d7ceb5050a10355a5aa1a9ea0b9d7ced7
Jacopo Grassi <jacopo.grassi@polito.it>
2026-02-26 15:20:29 +0100

    Add Sphinx configuration for documentation

---
+++ docs/conf.py
@@ -0,0 +1,17 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(".."))
+
+project = "AIUQ"
+author = "Jacopo Grassi"
+release = "0.0.2"
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",  # supporto Google/NumPy docstring
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+html_theme = "sphinx_rtd_theme"
